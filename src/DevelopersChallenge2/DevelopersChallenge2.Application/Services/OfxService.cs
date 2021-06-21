496769fe322e8ecad42f08ca99e08cb4e60d24bd
Denis Oliveira <86087037+denis-olvra@users.noreply.github.com>
2021-06-21 00:39:25 -0300

    Perform the code

--- src/DevelopersChallenge2/DevelopersChallenge2.Application/Services/OfxService.cs
+++ src/DevelopersChallenge2/DevelopersChallenge2.Application/Services/OfxService.cs
@@ -1,7 +1,6 @@
 ﻿using DevelopersChallenge2.Application.Domain.Entity;
 using DevelopersChallenge2.Application.Domain.ExtensionMethods;
 using DevelopersChallenge2.Application.Domain.Interfaces;
-using DevelopersChallenge2.Application.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System;
@@ -25,29 +24,73 @@ namespace DevelopersChallenge2.Application.Services
 
         public async Task ProcessOfxFiles(List<IFormFile> formFiles)
         {
-            //TODO improve log
-            long size = formFiles.Sum(f => f.Length);
+            _logger.LogInformation($"Start processing {formFiles.Count} Ofx files");
 
+            var transactionsGroupedByFile = new List<List<Transaction>>();
             foreach (var formFile in formFiles)
             {
-                if (formFile.Length > 0)
+                if (formFile.Length <= 0) continue;
+                
+                var fileName = Path.GetFileName(formFile.FileName);
+                var filePath = Path.Combine(Path.GetTempPath() + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss") + "-" + fileName);
+
+                await using (var stream = File.Create(filePath))
                 {
-                    var fileName = Path.GetFileName(formFile.FileName);
-                    var filePath = Path.Combine(Path.GetTempPath() + DateTime.UtcNow.ToString("yyyyMMdd-HHmmss") + "-" + fileName);
-                    
-                    using (var stream = File.Create(filePath))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-                    PersistsOfxTransactions(filePath);
+                    _logger.LogInformation($"Persisting the {fileName} file");
+                    await formFile.CopyToAsync(stream);
                 }
+
+                transactionsGroupedByFile.Add(GetTransactionsByOfxFile(filePath));
             }
+
+            PersistsOfxTransactions(transactionsGroupedByFile);
+            _logger.LogInformation($"End processing Ofx files");
+        }
+        
+        private List<Transaction> GetTransactionsByOfxFile(string filePath)
+        {
+            var ofxFile = filePath.ToOfx();
+            return ofxFile.Transactions;
         }
 
-        private void PersistsOfxTransactions(string filePath)
+        private void PersistsOfxTransactions(List<List<Transaction>> transactionsGroupedByFile)
+        {
+            _logger.LogInformation($"Start of persistence of transactions file.");
+            if (transactionsGroupedByFile.Count > 1)
+            {
+                var transactions = RemoveDuplicatedTransactions(transactionsGroupedByFile);
+                _transactionRepository.Save(transactions);
+                return;
+            }
+            _transactionRepository.Save(transactionsGroupedByFile[0]);
+            _logger.LogInformation($"End of persistence of transactions file.");
+        }
+
+        private List<Transaction> RemoveDuplicatedTransactions(List<List<Transaction>> transactionsGroupedByFile)
+        {
+            _logger.LogInformation("Start of duplicated transaction removal");
+            var transactions = new List<Transaction>();
+            for (var i = 0; i < transactionsGroupedByFile.Count-1; i++)
+            {
+                var currentTransactions = transactionsGroupedByFile[i];
+                var nextTransactions = transactionsGroupedByFile[i+1];
+
+                foreach (var transaction in currentTransactions)
+                {
+                    nextTransactions.RemoveAll(x => x.UniqueKey == transaction.UniqueKey);
+                }
+
+                transactions.AddRange(transactionsGroupedByFile[i].Union(transactionsGroupedByFile[i+1]));
+            }
+
+            _logger.LogInformation("End of duplicated transaction removal");
+            return transactions;
+        }
+
+        /*private void PersistsOfxTransactions(string filePath)
         {
             var fileName = Path.GetFileName(filePath);
-            _logger.LogInformation($"Start of ofx file conversion process. {fileName}");
+            _logger.LogInformation($"Start of persistence of transactions {fileName} file.");
 
             var ofxFile = filePath.ToOfx();
             if(ofxFile.Transactions != null)
@@ -55,10 +98,12 @@ namespace DevelopersChallenge2.Application.Services
                 var transactions = ofxFile.Transactions
                     .Select(x => { x.OfxFileReference = fileName; return x; })
                     .ToList();
+                
+                RemoveDuplicatedTransactions(transactions);
                 _transactionRepository.Save(transactions);
             }            
             
-            _logger.LogInformation($"End of ofx file conversion process. {fileName}");
-        }
+            
+        }*/
     }
 }
